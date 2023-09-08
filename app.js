d4e7d6ef0a286aa94c245266b423ddc8519de2c4
Meyer <meyer-hector@hotmail.com>
2023-09-08 14:26:44 -0300

    se configuro app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(morgan('dev'))
+
+import { conexionDB } from './db.js'
+conexionDB()
+
+
+// RUTAS
+
+
+
+//
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
