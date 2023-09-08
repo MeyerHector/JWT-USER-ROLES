bda2ec03e7b43b9ea41d4caa9be77b9b968dd497
Meyer <meyer-hector@hotmail.com>
2023-09-08 14:30:23 -0300

    se establecio la conexion con la base de datos

--- db.js
+++ db.js
@@ -1,1 +1,24 @@
 
+import { Sequelize, Model, DataTypes } from 'sequelize'
+
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT || "mysql",
+    }
+)
+
+export const conexionDB = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Conexión a la base de datos exitosa')
+    } catch (error) {
+        console.log('Error al conectar a la base de datos', error);
+    }
+
+}
+
+export { Model, DataTypes}
